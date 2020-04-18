427c5f4072442067fd85f3bed75a04cd4fe3faec
Chan Park <chanpark93@gmail.com>
2020-04-18 16:35:02 -0700

    update handleChange for custom form

--- frontend/src/client/Form/customForm.component.js
+++ frontend/src/client/Form/customForm.component.js
@@ -8,24 +8,37 @@ const CustomForm = ({state, handleSubmit, buttonLabel, ...otherProps}) => {
 
     // initial state is passed down prop
     // onChange, setFormState with user input
+    const handleChange = (e, ind, state ) => {
+        e.preventDefault()
+        console.log(e.target)
+        const newState = formState.map((obj, i) => {
+            for (let key in obj) {
+              if (e.target.name === key) {
+                  const newObj = {
+                      ...obj,
+                      [e.target.name]: e.target.value
+                  }
+                  return newObj
+              }
+              return obj
+            }
+        })
+=        setFormState(newState)
+    }
 
     // onSubmit, post formState
     console.log("formState", formState)
     return (
         <form onSubmit={handleSubmit}>
-            { formState.map((obj, i) => {
+            { formState.map((obj, i) => {                
                 for (let key in obj) {
-                    const handleChange = e => {
-                        e.preventDefault()
-
-                    }
                     return(
                         <FormInput 
                             label={key}
                             key={i}
                             name={key}
                             value={obj[key]}
-                            handleChange={handleChange}
+                            handleChange={(e) => handleChange(e, i, obj)}
                         />
                     )
                 }
